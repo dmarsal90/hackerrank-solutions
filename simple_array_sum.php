abd177e39fd2af8b26c9ac7bbfcad92c6d0637aa
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-05 15:33:50 -0400

    Add simple array sum

---
+++ simple_array_sum.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @title Simple Array Sum
+ * @link https://www.hackerrank.com/challenges/simple-array-sum
+ * @status solved
+ */
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum($ar) {
+    /*
+     * Write your code here.
+     */
+    return array_sum($ar);
+}
+?>
